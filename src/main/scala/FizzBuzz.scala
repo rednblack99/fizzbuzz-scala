b51ae989afb64334db0027a05d64602006cdba2e
Adam Lodowski <adamlodowski@gmail.com>
2019-03-31 17:13:44 +0100

    Added dummy test and made it pass

---
+++ src/main/scala/FizzBuzz.scala
@@ -0,0 +1,5 @@
+object FizzBuzz extends App {
+  def hello = {
+    "hello, world"
+  }
+}
---
+++ src/test/scala/FizzBuzzTest.scala
@@ -0,0 +1,5 @@
+class FizzBuzzTest extends org.scalatest.FunSuite {
+  test("FizzBuzz.hello") {
+    assert(FizzBuzz.hello === "hello, world")
+  }
+}
